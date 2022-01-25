29f24ee195ce54159f2c974f8c34de225266d834
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-25 15:36:53 -0300

    Update producto.service.ts
    Adicion de metodos

--- ProductosFront/src/app/service/producto.service.ts
+++ ProductosFront/src/app/service/producto.service.ts
@@ -1,9 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
 
-  constructor() { }
+  prodUrl = 'http://localhost:8080/productos/';
+
+  constructor(private hc: HttpClient) { }
+
+  public lista(): Observable<Producto[]> {
+    return this.hc.get<Producto[]>(this.prodUrl + 'listar');
+  }
+
+  public detallesID(id: number): Observable<Producto[]> {
+    return this.hc.get<Producto[]>(this.prodUrl + `detalles/${id}`);
+  }
+
+  public detallesNombre(nombre: string): Observable<Producto[]> {
+    return this.hc.get<Producto[]>(this.prodUrl + `detallesNombre/${nombre}`);
+  }
+
+  public save(prod: Producto): Observable<any> {
+    return this.hc.post<any>(this.prodUrl + `crear`, prod);
+  }
+
+  public update(id: number, prod: Producto): Observable<any> {
+    return this.hc.put<any>(this.prodUrl + `editar/${id}`, prod);
+  }
+
+  public delete(id: number): Observable<any> {
+    return this.hc.delete<any>(this.prodUrl + `borrar/${id}`);
+  }
 }
