e3a6b4da711b30f4c5a787047f5be048b31f5c5e
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-25 16:07:14 -0300

    Producto nuevo

--- ProductosFront/src/app/producto/nuevo-producto.component.ts
+++ ProductosFront/src/app/producto/nuevo-producto.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from '../models/producto';
+import { ProductoService } from '../service/producto.service';
 
 @Component({
   selector: 'app-nuevo-producto',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NuevoProductoComponent implements OnInit {
 
-  constructor() { }
+
+  nombre: string = '';
+  precio: number = 0;
+
+  constructor(
+    private prodServ: ProductoService,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onCreate(): void{
+    const prod = new Producto(this.nombre, this.precio);
+    this.prodServ.save(prod).subscribe (
+      data => {
+        this.toastr.success('Producto Creado', 'OK', {
+          timeOut: 3000, positionClass: 'toast-top-center'
+        });
+        this.router.navigate(['/']);
+      },
+      err => {
+        this.toastr.error(err.error.mensaje, 'Fail', {
+          timeOut: 3000,  positionClass: 'toast-top-center',
+        });
+        this.router.navigate(['/']);
+      }
+    );
   }
 
 }
