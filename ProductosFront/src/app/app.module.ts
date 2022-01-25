7c483aff1de51c4821c9f062e56b2a8519f51bb8
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-25 12:23:57 -0300

    Adiciones
    Creación de modelo, vistas y servicio.

--- ProductosFront/src/app/app.module.ts
+++ ProductosFront/src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { ListaProductoComponent } from './producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
