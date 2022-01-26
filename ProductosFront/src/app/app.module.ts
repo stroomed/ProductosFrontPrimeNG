d462ab4dd9aa4168352988101b2eb88033292208
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-26 11:23:13 -0300

    Creaciones
    Creación de módulo, componentes y servicio.

--- ProductosFront/src/app/app.module.ts
+++ ProductosFront/src/app/app.module.ts
@@ -6,9 +6,17 @@ import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
+import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
+import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
+import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaProductoComponent,
+    DetalleProductoComponent,
+    NuevoProductoComponent,
+    EditarProductoComponent
   ],
   imports: [
     BrowserModule,
