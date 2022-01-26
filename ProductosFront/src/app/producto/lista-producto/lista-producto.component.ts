6b0be32f61d1f25fabf3b3c8fd53d94ce2b35e05
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-26 12:24:11 -0300

    Genera Lista

--- ProductosFront/src/app/producto/lista-producto/lista-producto.component.ts
+++ ProductosFront/src/app/producto/lista-producto/lista-producto.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/service/producto.service';
+
+
 
 @Component({
   selector: 'app-lista-producto',
@@ -7,9 +12,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaProductoComponent implements OnInit {
 
-  constructor() { }
+  productos: Producto[] = [];
+
+  constructor(private prodServ: ProductoService, private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.cargarProductos();
+  }
+
+  cargarProductos(): void {
+    this.prodServ.lista().subscribe(
+      data => {
+        this.productos = data;
+      }, err => {
+        console.log(err);
+      }
+    );
+  }
+
+  borrarProducto(id: number){
+    this.prodServ.delete(id).subscribe(
+      data => {
+        this.toastr.success(
+          'Producto eliminado',
+          'OK',
+          {
+            timeOut: 3000,
+            positionClass: 'toast-top-center'
+          }
+        );
+        this.cargarProductos();
+      }, err => {
+        this.toastr.error(
+          err.error.mensaje,
+          'Fail',
+          {
+            timeOut: 3000,
+            positionClass: 'toast-top-center'
+          }
+        );
+      }
+    );
   }
 
 }
