75356317de33b77dfb2dd24b2896f86bf7ab341e
stroomed <ricardo.gutierrez.valejos@gmail.com>
2022-01-26 11:26:03 -0300

    Update app.module.ts

--- ProductosFront/src/app/app.module.ts
+++ ProductosFront/src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
 import { ListaProductoComponent } from './producto/lista-producto/lista-producto.component';
 import { DetalleProductoComponent } from './producto/detalle-producto/detalle-producto.component';
 import { NuevoProductoComponent } from './producto/nuevo-producto/nuevo-producto.component';
 import { EditarProductoComponent } from './producto/editar-producto/editar-producto.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +26,9 @@ import { EditarProductoComponent } from './producto/editar-producto/editar-produ
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
